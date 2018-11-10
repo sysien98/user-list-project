80712caa0a81d516e70234c11f889c3b268f5f86
Sylwia Sadowska <sylwiaxsadowska@gmail.com>
2018-11-10 16:05:41 +0100

    Updates

---
+++ precache-manifest.8c3e25b475bd35d7cbfbecb632ba4d4a.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "97e8f30e947a28562fc1",
+    "url": "/user-list-project/static/js/runtime~main.97e8f30e.js"
+  },
+  {
+    "revision": "c766f2da75a36de7a69b",
+    "url": "/user-list-project/static/js/main.c766f2da.chunk.js"
+  },
+  {
+    "revision": "3da870a3c1c4b41662f2",
+    "url": "/user-list-project/static/js/1.3da870a3.chunk.js"
+  },
+  {
+    "revision": "c766f2da75a36de7a69b",
+    "url": "/user-list-project/static/css/main.2fa0ca82.chunk.css"
+  },
+  {
+    "revision": "ec67b6d26ebdce6c5d5915bfb730b5f2",
+    "url": "/user-list-project/index.html"
+  }
+];
